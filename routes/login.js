22aec69c3f4213ce9ffa5b5f0386e1ff62d29373
Kabilesh <kabilesh.ravi@cielocosta.com>
2021-01-31 18:44:17 +0530

    admin login updated

--- routes/login.js
+++ routes/login.js
@@ -37,4 +37,38 @@ router.post(
   }
 );
 
+router.post(
+  "/admin",
+  [
+    body("id").not().isEmpty().isLength({ min: 4 }),
+    body("password").not().isEmpty().isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      console.log(req.body);
+      const errors = validationResult(req);
+      if (errors.isEmpty()) {
+        const User = await db.query(
+          "SELECT * from admincredentials WHERE id=$1",
+          [req.body.id]
+        );
+        console.log(User);
+        const Id = User.rows[0].id;
+        const password = User.rows[0].pass;
+        if (password === req.body.password) {
+          jwt.sign({ user: User.rows[0] }, "secretkey", (err, token) => {
+            res.json({ token: token, msg: "Login Successfull" });
+          });
+        } else {
+          res.send("Please Enter Proper Credentials");
+        }
+      } else {
+        res.send({ error: "Bad request,please check credentials" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
 module.exports = router;
