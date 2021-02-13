088602b0ea76825afe5e4bf6f964d419414901e2
Kabilesh <kabilesh.ravi@cielocosta.com>
2021-02-13 22:24:11 +0530

    filtering implemented

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
@@ -26,6 +27,36 @@ router.get("/", verifyToken, async (req, res) => {
     } else {
       const names = await db.query("SELECT regnumber FROM students");
       res.send(names.rows[0]);
+    }
+  });
+});
+
+router.get("/:id", verifyToken, async (req, res) => {
+  jwt.verify(req.token, "adminkey", async (err, authData) => {
+    if (err) {
+      res.send("Token error");
+    } else {
+      const id = req.params.id;
+      const response = await db.query(
+        "SELECT s.id,s.company_id,s.role_off,s.ctc_off,s.cgpa,s.date_alloted,c.company_name,c.mail_id FROM schedule s,company c  WHERE s.company_id=$1 AND s.company_id=c.id",
+        [id]
+      );
+      res.send(response.rows);
+    }
+  });
+});
+
+router.get("/student/:id", verifyToken, async (req, res) => {
+  jwt.verify(req.token, "adminkey", async (err, authData) => {
+    if (err) {
+      res.send("Token error");
+    } else {
+      const cgpa = req.params.id;
+      const response = await db.query(
+        "SELECT * FROM students s,department d where s.cgpa >= $1 AND s.dept_id=d.id",
+        [cgpa]
+      );
+      res.send(response.rows);
     }
   });
 });
